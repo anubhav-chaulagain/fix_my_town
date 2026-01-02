7b8f7bc838c8025e8c43b7ab904b9640c35a2cad
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-01-02 10:43:06 +0545

    create hive table constant of db and user table

---
+++ lib/core/constants/hive_table_constants.dart
@@ -0,0 +1,20 @@
+class HiveTableConstants {
+  HiveTableConstants._();
+
+  // Database name
+  static const String dbName = "fix_my_town_db";
+
+  static const int userTypeId = 0;
+  static const String userTable = "user_table";
+}
+  // static const int reportTypeId = 1;
+  // static const String reportTable = "report_table";
+
+  // static const int commentTypeId = 2;
+  // static const String commentTable = "comment_table";
+
+  // static const int categoryTypeId = 3;
+  // static const String categoryTable = "category_table";
+
+  // static const int settingsTypeId = 4;
+  // static const String settingsTable = "settings_table";

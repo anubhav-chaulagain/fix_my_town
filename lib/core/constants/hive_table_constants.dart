f12208d5927e8a457b979c854b3dbd0b13d206e2
anubhav-chaulagain <achaulagain.mail123@gmail.com>
2026-01-23 13:26:49 +0545

    category hive model and its adapter class

--- lib/core/constants/hive_table_constants.dart
+++ lib/core/constants/hive_table_constants.dart
@@ -6,6 +6,9 @@ class HiveTableConstant {
 
   static const int userTypeId = 0;
   static const String userTable = "user_table";
+
+  static const int categoryTypeId = 1;
+  static const String categoryTable = "category_table";
 }
   // static const int reportTypeId = 1;
   // static const String reportTable = "report_table";
@@ -13,8 +16,7 @@ class HiveTableConstant {
   // static const int commentTypeId = 2;
   // static const String commentTable = "comment_table";
 
-  // static const int categoryTypeId = 3;
-  // static const String categoryTable = "category_table";
+  
 
   // static const int settingsTypeId = 4;
   // static const String settingsTable = "settings_table";
